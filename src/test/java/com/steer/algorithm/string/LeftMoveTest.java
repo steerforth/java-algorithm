415e38e99424b7aabca7fcd308523e5829a68e7a
steer <565355716@qq.com>
2020-05-02 17:50:05 +0800

    反转字符串

---
+++ src/test/java/com/steer/algorithm/string/LeftMoveTest.java
@@ -0,0 +1,43 @@
+package com.steer.algorithm.string;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+/**
+ * 要求把字符串前k个字符，移到字符串尾部
+ * eg. 'abcdef'移动前2个，则新的为'cdefab'
+ */
+public class LeftMoveTest {
+
+    /**
+     * 思路:
+     * ab取镜像为ba
+     * cdef取镜像为fedc
+     * 则结果为bafedc的镜像，即cdefab
+     */
+    @Test
+    public void test(){
+        //移动2位
+        char[] origin = new char[]{'a','b','c','d','e','f'};
+        int size = 2;
+
+        reverseString(origin,0,size-1);
+        reverseString(origin,size,origin.length-1);
+        reverseString(origin,0,origin.length-1);
+
+        System.out.println(Arrays.toString(origin));
+
+    }
+
+    private void reverseString(char[] chars,int from, int to){
+        while (from < to){
+            char temp = chars[from];
+            chars[from] = chars[to];
+            chars[to] = temp;
+            to--;
+            from++;
+        }
+    }
+
+}
